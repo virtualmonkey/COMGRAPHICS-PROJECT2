ac190efdb315d4a306d0190972aa2b5cf16f3afd
virtualmonkey <luis212urbinau@gmail.com>
2020-10-17 14:52:25 -0600

    Final changes made

--- RayTracer.py
+++ RayTracer.py
@@ -9,7 +9,7 @@ import random
 brick = Material(diffuse = color(0.8, 0.25, 0.25 ), spec = 16)
 stone = Material(diffuse = color(0.4, 0.4, 0.4 ), spec = 32)
 mirror = Material(spec = 64, matType = REFLECTIVE)
-glass = Material(spec = 64, ior = 1.5, matType= TRANSPARENT) 
+glass = Material(spec = 64, ior = 1.5, matType= TRANSPARENT)
 
 boxMat = Material(texture = Texture('./materials/box.bmp'))
 obsidianMat = Material(texture= Texture('./materials/obsidian.bmp'))
@@ -31,19 +31,17 @@ r.glClear()
 r.envmap = Envmap('./materials/sunset.bmp')
 
 # Lights
-#r.pointLights.append( PointLight(position = V3(-4,4,0), intensity = 0.5))
-#r.pointLights.append( PointLight(position = V3( 4,0,0), intensity = 0.5))
-r.dirLight = DirectionalLight(direction = V3(1, -1, -2), intensity = 0.5)
+r.dirLight = DirectionalLight(direction = V3(1, -1, -1), intensity = 0.5)
 r.ambientLight = AmbientLight(strength = 0.1)
 
 # Objects
 r.scene.append( AABB(V3(0, -1, -5), V3(10,0.5,10) , dirtMat))
 
 
-r.scene.append( AABB(V3(-1, -0.5, -5), V3(0.5,0.5,0.5) , mirror))
+r.scene.append( AABB(V3(-1, -0.5, -5), V3(0.5,0.5,0.5) , obsidianMat))
 r.scene.append( AABB(V3(-0.5, -0.5, -5), V3(0.5,0.5,0.5) , obsidianMat))
 r.scene.append( AABB(V3(0, -0.5, -5), V3(0.5,0.5,0.5) , obsidianMat))
-r.scene.append( AABB(V3(0.5, -0.5, -5), V3(0.5,0.5,0.5) , mirror))
+r.scene.append( AABB(V3(0.5, -0.5, -5), V3(0.5,0.5,0.5) , obsidianMat))
 
 r.scene.append( AABB(V3(-1, 0, -5), V3(0.5,0.5,0.5) , obsidianMat))
 r.scene.append( AABB(V3(-1, 0.5, -5), V3(0.5,0.5,0.5) , obsidianMat))
@@ -53,21 +51,26 @@ r.scene.append( AABB(V3(0.5, 0, -5), V3(0.5,0.5,0.5) , obsidianMat))
 r.scene.append( AABB(V3(0.5, 0.5, -5), V3(0.5,0.5,0.5) , obsidianMat))
 r.scene.append( AABB(V3(0.5, 1, -5), V3(0.5,0.5,0.5) , obsidianMat))
 
-r.scene.append( AABB(V3(-1, 1.5, -5), V3(0.5,0.5,0.5) , mirror))
+r.scene.append( AABB(V3(-1, 1.5, -5), V3(0.5,0.5,0.5) , obsidianMat))
 r.scene.append( AABB(V3(-0.5,  1.5, -5), V3(0.5,0.5,0.5) , obsidianMat))
 r.scene.append( AABB(V3(0, 1.5, -5), V3(0.5,0.5,0.5) , obsidianMat))
-r.scene.append( AABB(V3(0.5, 1.5, -5), V3(0.5,0.5,0.5) , mirror))
+r.scene.append( AABB(V3(0.5, 1.5, -5), V3(0.5,0.5,0.5) , obsidianMat))
 
-r.scene.append( AABB(V3(-0.25, 0.5, -5), V3(1,1.5,0.5) , purpleMat))
-r.scene.append( AABB(V3(-0.17, 0.4, -3), V3(1,1,0.01) , glass))
-
-r.scene.append( AABB(V3(0.75, -0.5, -4), V3(0.5,0.5,0.5) , pumpkinMat))
-
+r.scene.append( AABB(V3(-0.5, 1, -5), V3(0.5,0.5,0.5) , obsidianMat))
+r.scene.append( AABB(V3(0, 1, -5), V3(0.5,0.5,0.5) , glass))
+r.scene.append( AABB(V3(0, 0.5, -5), V3(0.5,0.5,0.5) , obsidianMat))
+r.scene.append( AABB(V3(-0.5, 0.5, -5), V3(0.5,0.5,0.5) , glass))
+r.scene.append( AABB(V3(-0.5, 0, -5), V3(0.5,0.5,0.5) , obsidianMat))
+r.scene.append( AABB(V3(0, 0, -5), V3(0.5,0.5,0.5) , glass))
 
 r.scene.append( AABB(V3(-1.5, -0.5, -3), V3(0.5,0.5,0.5) , bigOakMat ))
 r.scene.append( AABB(V3(-1.5, 0, -3), V3(0.5,0.5,0.5) , bigOakMat ) )
 r.scene.append( AABB(V3(-1.5, 0.5, -3), V3(0.5,0.5,0.5) , bigOakMat ))
 r.scene.append( AABB(V3(-1.5, 1, -3), V3(0.5,0.5,0.5) , bigOakMat ))
+
+r.scene.append( AABB(V3(-1, -0.5, -4), V3(0.7,0.6,0.1) , mirror))
+r.scene.append( AABB(V3(-1, -0.5, -3), V3(0.5,0.5,0.5) , pumpkinMat))
+r.scene.append( AABB(V3(-1.25, -0.5, -2), V3(0.5,0.5,0.5) , pumpkinMat))
 
 r.scene.append( AABB(V3(-2, 1, -3), V3(0.5,0.5,0.5) , leavesMat ) )
 r.scene.append( AABB(V3(-1.5, 1, -3.5), V3(0.5,0.5,0.5) , leavesMat ) )
@@ -75,7 +78,21 @@ r.scene.append( AABB(V3(-1, 1, -3), V3(0.5,0.5,0.5) , leavesMat ) )
 r.scene.append( AABB(V3(-1.5, 1, -2.5), V3(0.5,0.5,0.5) , leavesMat ) )
 r.scene.append( AABB(V3(-1.5, 1.5, -3), V3(0.5,0.5,0.5) , leavesMat ) )
 
+r.scene.append( AABB(V3(1.5, -0.5, -3), V3(0.5,0.5,0.5) , bigOakMat ))
+r.scene.append( AABB(V3(1.5, 0, -3), V3(0.5,0.5,0.5) , bigOakMat ) )
+r.scene.append( AABB(V3(1.5, 0.5, -3), V3(0.5,0.5,0.5) , bigOakMat ))
+r.scene.append( AABB(V3(1.5, 1, -3), V3(0.5,0.5,0.5) , bigOakMat ))
 
+r.scene.append( AABB(V3(1, -0.5, -4), V3(0.75,0.6,0.1) , mirror))
+
+r.scene.append( AABB(V3(1, -0.5, -3), V3(0.5,0.5,0.5) , pumpkinMat))
+r.scene.append( AABB(V3(1.25, -0.5, -2), V3(0.5,0.5,0.5) , pumpkinMat))
+
+r.scene.append( AABB(V3(1, 1, -3), V3(0.5,0.5,0.5) , leavesMat ) )
+r.scene.append( AABB(V3(1.5, 1, -3.5), V3(0.5,0.5,0.5) , leavesMat ) )
+r.scene.append( AABB(V3(2, 1, -3), V3(0.5,0.5,0.5) , leavesMat ) )
+r.scene.append( AABB(V3(1.5, 1, -2.5), V3(0.5,0.5,0.5) , leavesMat ) )
+r.scene.append( AABB(V3(1.5, 1.5, -3), V3(0.5,0.5,0.5) , leavesMat ) )
 
 r.rtRender()
 
